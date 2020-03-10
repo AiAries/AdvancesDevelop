6ccbab1200c24043645061abd926946b29964a92
FreedomPeace <18658251543@163.com>
2020-03-11 00:18:45 +0800

    添加kotlin学习工程

---
+++ lib-kotlin-learn/src/main/java/com/example/lib_kotlin_learn/MyClass.kt
@@ -0,0 +1,13 @@
+package com.example.lib_kotlin_learn
+
+public class MyClass {
+    fun main(args: Array<String>) {
+        println(66)
+    }
+    fun add(a:Int ,b:Int):Int
+    {
+        return a - b
+    }
+
+
+}
