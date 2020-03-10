4a97c1f4421fb43930fedb437b174fb7fd6516c4
azp <kjl@163.com>
2020-03-10 13:54:44 +0800

    QRdemo，优化初始化问题

--- common_app/qrmodule/src/main/java/com/example/qrmodule/MainActivity.kt
+++ common_app/qrmodule/src/main/java/com/example/qrmodule/MainActivity.kt
@@ -1,23 +1,23 @@
 package com.example.qrmodule
 
-import android.support.v7.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Toast
 import com.example.qrmodule.zxing.ZXingScannerView
 
 class MainActivity : AppCompatActivity(),ZXingScannerView.ResultHandler {
 
-    private val zXingScannerView: ZXingScannerView
-        get() {
-            var mScannerView = ZXingScannerView(this)
-            return mScannerView
-        }
+    private lateinit var  zXingScannerView: ZXingScannerView
 
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var mScannerView = zXingScannerView
-        setContentView(mScannerView)
+        zXingScannerView = ZXingScannerView(this)
+        setContentView(zXingScannerView)
     }
 
     override fun handleResult(rawResult: com.google.zxing.Result?) {
