7a61a8d00c0d909ef97b3e4f3df41f6eb1a5ea96
FreedomPeace <18658251543@163.com>
2020-03-13 22:12:02 +0800

    添加Class语法学习

---
+++ lib-kotlin-learn/src/test/kotlin/com/example/lib_kotlin_learn/ClassLearnTest.kt
@@ -0,0 +1,18 @@
+package com.example.lib_kotlin_learn
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ClassLearnTest {
+
+    @Test
+    fun getFirstProperty() {
+        var a = ClassLearn("aizp")
+        println(a.toString())
+    }
+
+    @Test
+    fun getSecondProperty() {
+    }
+}
