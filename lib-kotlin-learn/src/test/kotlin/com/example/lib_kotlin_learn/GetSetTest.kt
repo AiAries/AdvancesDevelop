376728941c3dda700fa9da21c849adf026f70795
FreedomPeace <18658251543@163.com>
2020-03-15 19:53:03 +0800

    添加重写get set方法

---
+++ lib-kotlin-learn/src/test/kotlin/com/example/lib_kotlin_learn/GetSetTest.kt
@@ -0,0 +1,24 @@
+package com.example.lib_kotlin_learn
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class GetSetTest {
+
+    @Before
+    fun setUp() {
+
+    }
+    @Test
+    fun set(){
+        var obj = GetSet()
+        println(obj.age)
+        obj.age = -1
+        println(obj.age)
+        obj.age = 16
+        println(obj.age)
+    }
+
+}
