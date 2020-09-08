566e20f06d5e1bf25a7926b12849d3ebae319c14
aizhiping <aizp@bankcomm.com>
2020-09-08 10:59:17 +0800

    添加dragger rxjava 两个工程

---
+++ common_app/dagger_demo/src/main/java/com/example/dagger_demo/DaggerMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dagger_demo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DaggerMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dragger_main)
+    }
+}
---
+++ common_app/rxjava_demo/src/main/java/com/example/rxjava_demo/RxjavaMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rxjava_demo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class RxjavaMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rxjava_main)
+    }
+}
